934dd46faf4b6a3018d6fa0aef7418c59164d15b
Jeff Santucci <jeffrey.santucci@gmail.com>
2023-02-22 18:38:27 -0500

    Roshambo

---
+++ Roshambo/Player.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roshambo
+{
+    abstract class Player  // Base class (parent)
+    {
+        public string name { get; set; }
+        public virtual void RoshamboValue()
+        {
+            Console.WriteLine("You have chosen ");
+        }
+        public string choice { get; set; }
+
+        public void GenerateRoshambo()
+        {
+            Random rd = new Random();
+
+            int rand_num = rd.Next(1, 3);
+
+            if (rand_num == 1)
+            {
+                choice = "Rock";
+            }
+            else if (rand_num == 2)
+            {
+                choice = "Paper";
+            }
+            else
+
+                choice = "Scissors";
+        }
+
+    }                   
+        
+    
+    class Rock : Player
+    {
+        public override void RoshamboValue()
+        {
+            Console.WriteLine(name + " has selected " + choice);
+        }
+    }
+    
+    class Any : Player
+    {
+              
+        public override void RoshamboValue() 
+        {
+
+            GenerateRoshambo(); 
+            Console.WriteLine(name + " has selected " + choice + "!");
+            
+        }
+
+             
+    }
+  
+    
+    class Human : Player
+    {
+       
+        public override void RoshamboValue()
+        {
+                       
+            Console.WriteLine(name + " has selected " + choice + "!");
+            
+        }
+    }
+   
+}
---
+++ Roshambo/Program.cs
@@ -0,0 +1,108 @@
+﻿// Roshambo App
+// Jeff Santucci
+// 02/20/2023
+
+using Roshambo;
+using System.Xml.Serialization;
+using static Roshambo.Player;
+Human human = new Human();
+Rock rock = new Rock();
+Any random = new Any();
+
+
+
+string[] RoshamboArray = new string[3];
+
+RoshamboArray[0] = "Rock";
+RoshamboArray[1] = "Paper";
+RoshamboArray[2] = "Scissors";
+
+bool isOpponent = false;
+string opponentName;
+
+do
+{
+    Console.Write("Enter your name: ");
+    string playerName = Console.ReadLine();
+    do
+    {
+        Console.WriteLine("Select your Opponent either Rock or Random");
+        opponentName = Console.ReadLine();
+        if (opponentName == "Rock" || opponentName == "Random")
+        {
+            isOpponent = true;
+        }
+    } while (isOpponent == false); 
+   
+            for (int i = 0; i < RoshamboArray.Length; i++)
+            {
+                Console.WriteLine($"{i + 1}.{RoshamboArray[i]}");
+            }
+            Console.WriteLine(playerName + " choose one - 1. Rock, 2. Paper or 3. Scissors");
+            int num = int.Parse(Console.ReadLine());
+            string answer = RoshamboArray[num - 1];
+
+            human.choice = answer;
+            human.name = playerName;
+            human.RoshamboValue();
+
+            if (opponentName == "Rock")
+            {
+                rock.name = "Rock";
+                rock.choice = "Rock!";
+                rock.RoshamboValue();
+
+                if (human.choice == "Paper")
+                {
+                    Console.WriteLine(human.name + " is a winner! Paper beats Rock!");
+                }
+                else if (human.choice == "Rock")
+                {
+                    Console.WriteLine(human.name + " and " + rock.name + " Tied!");
+                }
+                else
+                {
+                    Console.WriteLine(rock.name + " wins! Rock beats " + human.choice + "!");
+                }
+            }
+            else if (opponentName == "Random")
+            {
+                random.name = "Random";
+                random.RoshamboValue();
+
+                if (human.choice == random.choice)
+                {
+                    Console.WriteLine(human.name + " and " + random.name + " Tied!");
+                }
+                else if (human.choice == "Paper" && random.choice == "Rock")
+                {
+                    Console.WriteLine(human.name + " is a winner! " + human.choice + " beats " + random.choice + "!");
+                }
+                else if (human.choice == "Rock" && random.choice == "Scissors")
+                {
+                    Console.WriteLine(human.name + " is a winner! " + human.choice + " beats " + random.choice + "!");
+                }
+                else if (human.choice == "Scissors" && random.choice == "Paper")
+                {
+                    Console.WriteLine(human.name + " is a winner! " + human.choice + " beats " + random.choice + "!");
+                }
+                else if (human.choice == "Rock" && random.choice == "Paper")
+                {
+                    Console.WriteLine(random.name + " is a winner! " + random.choice + " beats " + human.choice + "!");
+                }
+                else if (human.choice == "Scissors" && random.choice == "Rock")
+                {
+                    Console.WriteLine(random.name + " is a winner! " + random.choice + " beats " + human.choice + "!");
+                }
+                else
+                {
+                    Console.WriteLine(random.name + " is a winner! " + random.choice + " beats " + human.choice + "!");
+                }
+
+            }
+        
+   Console.WriteLine("Play Again? y/n");
+   
+
+} while (Console.ReadLine()=="y");
+Console.WriteLine("Thanks for playing .... Goodbye!");
